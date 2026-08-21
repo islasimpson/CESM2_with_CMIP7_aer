3328bbca4179a7cb6fbeaf0cc7e0b6a1762e28cb
Isla Simpson <islas@ucar.edu>
2026-08-21 16:23:23 -0600

    updating to include so4_bmb

--- utils/molecular_weights_diameters_densities.py
+++ utils/molecular_weights_diameters_densities.py
@@ -7,6 +7,7 @@ mol_weights={'bc_a4':12,
              'so4_a1_ag': 115,
              'so4_a1_ship': 115,
              'so4_a1_slv_was': 115,
+             'so4_a1_bmb': 115,
              'so4_a2': 115,
              'pom_a4': 12,
              'SOAG': 12,
@@ -24,6 +25,7 @@ particle_diams_cmip7={'bc_a4': 0.134e-6,
                  'so4_a1_ag': 0.134e-6, # agriculture
                  'so4_a1_ship': 0.261e-6, # shipping
                  'so4_a1_slv_was': 0.261e-6, # solvents + waste
+                 'so4_a1_bmb': 0.134e-6, # biomass burning  
                  'so4_a2': 0.0504e-6, # residential + transport
                  'pom_a4': 0.134e-6}
 
@@ -32,14 +34,16 @@ particle_diams_cmip6={'bc_a4': 0.134e-6,
                  'so4_a1_ag': 0.134e-6, # agriculture
                  'so4_a1_ship': 0.261e-6, # shipping
                  'so4_a1_slv_was': 0.134e-6, # solvents + waste
+                 'so4_a1_bmb': 0.134e-6, # biomass burning
                  'so4_a2': 0.0504e-6, # residential + transport
                  'pom_a4': 0.134e-6}
 
-# list of molecular densities
+# list of particle densities
 particle_dens = {'bc_a4': 1700.,
             'so4_a1_ene_ind': 1770., # energy + industry vertically distributed
             'so4_a1_ag': 1770., # agriculture
             'so4_a1_ship': 1770. , # shipping
             'so4_a1_slv_was': 1770. , # solvents + waste
+            'so4_a1_bmb': 1770, # biomass burning
             'so4_a2': 1770., # residential + transport
             'pom_a4': 1000.}
